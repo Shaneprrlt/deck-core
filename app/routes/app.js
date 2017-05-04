78aac6979c77b2f60d59ecf84ca6141e5f2c17e7
Shane Perreault <shaneprrlt@gmail.com>
2017-05-04 09:28:36 -0500

    fixed nesting for app index

--- app/routes/app.js
+++ app/routes/app.js
@@ -3,6 +3,21 @@ import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-rout
 
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
 
-  authenticationRoute: 'setup.index'
+  authenticationRoute: 'setup.index',
+
+  coach: Ember.inject.service(),
+  store: Ember.inject.service(),
+
+  model() {
+    let subdomain = this.get('coach.subdomain');
+    return Ember.RSVP.hash({
+      team: this.get('store').queryRecord('team', { subdomain: subdomain }),
+      labels: this.get('store').findAll('label'),
+      cards: this.get('store').findAll('card'),
+      apps: this.get('store').findAll('app'),
+      decks: this.get('store').findAll('deck'),
+      notifications: this.get('store').findAll('notification')
+    });
+  }
 
 });
